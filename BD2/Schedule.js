fa5f0436506a2108872de923dd821551d57d1ac3
Lariany-Yana <PassWord88908>
2026-06-02 15:16:20 +0300

    02.06.26 15:16 Schedule update

--- BD2/Schedule.js
+++ BD2/Schedule.js
@@ -1,66 +1,70 @@
 const scheduleDatabase = [
-  {//Понедельник
+  //#region Понедельник
+  {
     nameRu: "Стрим невозможен :[",
     scheduleRemark: "",
     scheduleType: "schedule-current",
-    date: "01.06.26"
+    date: "01.06.26",
   },
-  {//Вторник
-    nameRu: "Ван Пис",
-    nameEn: "One Piece",
-    scheduleRemark: "Начало в 15:00 по МСК",
-    scheduleType: "schedule-current",
-    date: "02.06.26"
-  },
-  {//Вторник
+  //#endregion
+  //#region Вторник
+  {
     nameRu: "Паук-Нуар",
     nameEn: "Spider-Noir",
     scheduleRemark: "Начало в 17:00 по МСК",
     scheduleType: "schedule-current",
-    date: "02.06.26"
+    date: "02.06.26",
   },
-  {//Вторник
+  {
     nameRu: "Покер",
     nameEn: "",
     scheduleRemark: "Начало в 21:00 по МСК",
     scheduleType: "schedule-current",
-    date: "02.06.26"
+    date: "02.06.26",
   },
-  {//Среда
+  //#endregion
+  //#region Среда
+  {
     nameRu: "Стрим маловероятен :|",
     scheduleRemark: "",
     scheduleType: "schedule-current",
-    date: "03.06.26"
+    date: "03.06.26",
   },
-  {//Четверг
+  //#endregion
+  //#region Четверг
+  {
     nameRu: "Стрим маловероятен :|",
     scheduleRemark: "",
     scheduleType: "schedule-current",
-    date: "04.06.26"
+    date: "04.06.26",
   },
-  {//Пятница
+  {
     nameRu: "Стрим маловероятен :|",
     scheduleRemark: "",
     scheduleType: "schedule-current",
-    date: "05.06.26"
+    date: "05.06.26",
   },
-  {//Суббота
+  //#endregion
+  //#region Суббота
+  {
     nameRu: "Стрим невозможен :[",
     scheduleType: "schedule-current",
-    date: "06.06.26"
+    date: "06.06.26",
   },
-  {//Воскресенье
+  //#endregion
+  //#region Воскресенье
+  {
     nameRu: "Стрим вполне вероятен :]",
     scheduleRemark: "",
     scheduleType: "schedule-current",
-    date: "07.06.26"
+    date: "07.06.26",
   },
-
-	// Старые дни
+  //#endregion
+  //#region Старое
   {
     nameRu: "Старое",
     scheduleRemark: "",
     scheduleType: "schedule-past",
-    date: "01.01.25"
+    date: "01.01.25",
   },
 ];
