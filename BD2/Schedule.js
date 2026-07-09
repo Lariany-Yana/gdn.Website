507cb4decfb34aeb4647e913c47a72583306abfd
Lariany-Yana <PassWord88908>
2026-07-09 16:10:44 +0300

    09.07.26 16:10 Schedule update

--- BD2/Schedule.js
+++ BD2/Schedule.js
@@ -48,21 +48,7 @@ const scheduleDatabase = [
   },
   // Четверг
   {
-    nameRu: "Warhammer 40 000: Rogue Trader",
-    scheduleRemark: "Игра × Начало в 16:00 по МСК",
-    scheduleType: "schedule-current",
-    date: "09.07.26",
-  },
-  {
-    nameRu: "Tainted Grail: The Fall of Avalon",
-    scheduleRemark: "Игра × Начало в 18:00 по МСК",
-    scheduleType: "schedule-current",
-    date: "09.07.26",
-  },
-  {
-    nameRu: "Пока не сыграл в ящик",
-    nameEn: "The Bucket List",
-    scheduleRemark: "Фильм × Начало в 20:00 по МСК",
+    nameRu: "Стрим не состоялся",
     scheduleType: "schedule-current",
     date: "09.07.26",
   },
@@ -132,7 +118,21 @@ const scheduleDatabase = [
   },
   // Вторник
   {
-    nameRu: "Стрим вполне вероятен",
+    nameRu: "Warhammer 40 000: Rogue Trader",
+    scheduleRemark: "Игра × Начало в 16:00 по МСК",
+    scheduleType: "schedule-current",
+    date: "14.07.26",
+  },
+  {
+    nameRu: "Tainted Grail: The Fall of Avalon",
+    scheduleRemark: "Игра × Начало в 18:00 по МСК",
+    scheduleType: "schedule-current",
+    date: "14.07.26",
+  },
+  {
+    nameRu: "Пока не сыграл в ящик",
+    nameEn: "The Bucket List",
+    scheduleRemark: "Фильм × Начало в 20:00 по МСК",
     scheduleType: "schedule-current",
     date: "14.07.26",
   },
